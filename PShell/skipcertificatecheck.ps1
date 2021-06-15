6a76091dfcbe54ea3cba6562869d3c699ed85f2a
MLSeven <59027302+MLSeven@users.noreply.github.com>
2021-06-15 09:01:46 +0100

    Create skipcertificatecheck.ps1

---
+++ PShell/skipcertificatecheck.ps1
@@ -0,0 +1,35 @@
+$certCallback = @"
+    using System;
+    using System.Net;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
+    public class ServerCertificateValidationCallback
+    {
+        public static void Ignore()
+        {
+            if(ServicePointManager.ServerCertificateValidationCallback ==null)
+            {
+                ServicePointManager.ServerCertificateValidationCallback += 
+                    delegate
+                    (
+                        Object obj, 
+                        X509Certificate certificate, 
+                        X509Chain chain, 
+                        SslPolicyErrors errors
+                    )
+                    {
+                        return true;
+                    };
+            }
+        }
+    }
+"@
+
+if (-not ([System.Management.Automation.PSTypeName]'ServerCertificateValidationCallback').Type) {
+    # Ignore Self Signed SSL via a custom type
+
+    Add-Type $certCallback
+}
+
+[ServerCertificateValidationCallback]::Ignore()
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
